3df188d00da8bf88272913ce9a700122721184fa
Sapronov Konstantin <ks.costas@gmail.com>
2023-11-27 19:08:13 +0300

    feat: add  type assertion for default value

--- src/features/counter/contexts/counter-context.tsx
+++ src/features/counter/contexts/counter-context.tsx
@@ -2,7 +2,11 @@ import { createContext, useContext } from 'react';
 
 import { CounterStore } from '../stores/counter-store';
 
-export const CounterContext = createContext<CounterStore>(null!);
+/**
+ * Use an empty object as default value and cast it to the expected context type
+ * https://react-typescript-cheatsheet.netlify.app/docs/basic/getting-started/context/#type-assertion-as-an-alternative
+ */
+export const CounterContext = createContext<CounterStore>({} as CounterStore);
 
 export const useCounter = () => {
   const counter = useContext(CounterContext);
