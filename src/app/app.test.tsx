288e64bf7ca1c088efc63822f42aeabe2d00a0e0
Sapronov Konstantin <ks.costas@gmail.com>
2023-11-28 10:40:58 +0300

    refactor: mock dependencies

--- src/app/app.test.tsx
+++ src/app/app.test.tsx
@@ -1,6 +1,15 @@
+import { JSX, PropsWithChildren } from 'react';
 import { render, screen } from '@testing-library/react';
 
 import { App } from './app';
+
+jest.mock('@/features/counter', () => ({
+  Counter: (): JSX.Element => <h1>It renders App component</h1>,
+}));
+
+jest.mock('@/providers/app-provider', () => ({
+  AppProvider: ({ children }: PropsWithChildren): JSX.Element => <div>{children}</div>,
+}));
 
 describe('App', () => {
   it('renders App component', async () => {
@@ -10,7 +19,7 @@ describe('App', () => {
     // ACT
     const headingContainer = await screen.getByRole('heading', {
       level: 1,
-      name: 'MobX and React Context',
+      name: 'It renders App component',
     });
 
     // ASSERT
