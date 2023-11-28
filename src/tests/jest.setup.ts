63d9803bcdcf7f7eff9cf672fa9a2544a6b1103c
Sapronov Konstantin <ks.costas@gmail.com>
2023-11-28 16:52:07 +0300

    fix: remove console mock

--- src/tests/jest.setup.ts
+++ src/tests/jest.setup.ts
@@ -4,7 +4,3 @@
 // learn more: https://github.com/testing-library/jest-dom
 
 import '@testing-library/jest-dom';
-
-beforeAll(() => {
-  jest.spyOn(global.console, 'log').mockImplementation();
-});
