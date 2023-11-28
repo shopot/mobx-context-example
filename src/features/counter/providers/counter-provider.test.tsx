ac843833d96c74553d88d15cf4520a0fecb0f373
Sapronov Konstantin <ks.costas@gmail.com>
2023-11-28 16:52:36 +0300

    feat: add test

---
+++ src/features/counter/providers/counter-provider.test.tsx
@@ -0,0 +1,41 @@
+import { useContext } from 'react';
+import { render, screen } from '@testing-library/react';
+
+import { CounterStore } from '../stores/counter-store';
+import { CounterProvider } from './counter-provider';
+import { CounterContext } from '../contexts/counter-context';
+
+jest.mock('../stores/counter-store');
+
+const TestingComponent = () => {
+  const { value } = useContext(CounterContext);
+
+  return <div data-testid="value-id">{value}</div>;
+};
+
+describe('CounterProvider', () => {
+  beforeAll(() => {
+    (CounterStore as jest.Mock).mockImplementation(() => {
+      return {
+        value: 333,
+      };
+    });
+  });
+
+  it('should', async () => {
+    // ARRANGE
+
+    render(
+      <CounterProvider>
+        <TestingComponent />
+      </CounterProvider>
+    );
+
+    // ACT
+    const valueContainer = await screen.getByTestId('value-id');
+
+    // ASSERT
+    expect(valueContainer).toBeInTheDocument();
+    expect(valueContainer.textContent).toEqual('333');
+  });
+});
